9d002c9c8b1631054cba93979c951c6afdfebd6d
Yong Cong Sin <1563974+imbawind@users.noreply.github.com>
2019-07-26 19:24:45 +1000

    Remove useless files

--- dev/set_debug_flags.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-SCRIPTDIR=$(dirname "$(readlink -f "$0")")
-grep -rl "static const bool debug = false;" "$SCRIPTDIR/../core/include" "$SCRIPTDIR/../master/include" "$SCRIPTDIR/../ros_bridge/include" | xargs sed -i 's/static const bool debug = false;/static const bool debug = true;/g'
--- dev/unset_debug_flags.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-SCRIPTDIR=$(dirname "$(readlink -f "$0")")
-grep -rl "static const bool debug = true;" "$SCRIPTDIR/../core/include" "$SCRIPTDIR/../master/include" "$SCRIPTDIR/../ros_bridge/include" | xargs sed -i 's/static const bool debug = true;/static const bool debug = false;/g'
